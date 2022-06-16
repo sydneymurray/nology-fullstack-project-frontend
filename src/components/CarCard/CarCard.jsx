f87b4f950894c5fc8fc63ace4428e424ed300468
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-06-16 14:52:08 +0100

    Working on the Cards

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -10,8 +10,12 @@ const CarCard = ({car}) => {
 
   return <>
     <div className="car-card" onClick={handleClick}>
-        <img className="car-card__image" src={car.mainImageURL} alt={car.manufacturer + " " + car.model}/>
-        <h2 className="car-card__h2">{car.manufacturer + " " + car.model}</h2>
+      <h3 className="car-card__heading">{car.manufacturer + " " + car.model}</h3>
+      <img className="car-card__image" src={car.mainImageURL} alt={car.manufacturer + " " + car.model}/>
+      <p className="car-card__detail-item"><b>Engine Capacity:  </b>{car.engineCapacity}</p>
+      <p className="car-card__detail-item"><b>Mileage:  </b>{car.mileage}</p>
+      <p className="car-card__detail-item"><b>Manufactured:  </b>{car.year}</p>
+      <p className="car-card__detail-item"><b>Price:  $</b>{car.price}</p>
     </div>
   </>
 }
